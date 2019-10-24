55fed803e9baa4d57600fe83da1a49405063ef94
James Thesken <jthesken@hawaii.edu>
2019-10-23 20:23:53 -1000

    Rendezvous works across networks

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ This is both an educational project and something I hope could be useful for an 
 ## High-level Goals:
 1. Use p2p technology.
 2. Encrypt messages between senders.
-
+3. Make a friendly interface.
+4. ???
+5. Profit
 
 ## Thank you!
 Credit to: [libp2p](https://github.com/libp2p/go-libp2p-examples/tree/master/chat-with-mdns) who provided the example which will serve as the groundwork for MonkSeal.
--- flags.go
+++ flags.go
@@ -2,23 +2,62 @@ package main
 
 import (
 	"flag"
+	"strings"
+
+	dht "github.com/libp2p/go-libp2p-kad-dht"
+	maddr "github.com/multiformats/go-multiaddr"
 )
 
-type config struct {
-	RendezvousString string
-	ProtocolID       string
-	listenHost       string
-	listenPort       int
+// A new type we need for writing a custom flag parser
+type addrList []maddr.Multiaddr
+
+func (al *addrList) String() string {
+	strs := make([]string, len(*al))
+	for i, addr := range *al {
+		strs[i] = addr.String()
+	}
+	return strings.Join(strs, ",")
 }
 
-func parseFlags() *config {
-	c := &config{}
+func (al *addrList) Set(value string) error {
+	addr, err := maddr.NewMultiaddr(value)
+	if err != nil {
+		return err
+	}
+	*al = append(*al, addr)
+	return nil
+}
 
-	flag.StringVar(&c.RendezvousString, "rendezvous", "meetme", "Unique string to identify group of nodes. Share this with your friends to let them connect with you")
-	flag.StringVar(&c.listenHost, "host", "0.0.0.0", "The bootstrap node host listen address\n")
-	flag.StringVar(&c.ProtocolID, "pid", "/chat/1.1.0", "Sets a protocol id for stream headers")
-	flag.IntVar(&c.listenPort, "port", 4001, "node listen port")
+func StringsToAddrs(addrStrings []string) (maddrs []maddr.Multiaddr, err error) {
+	for _, addrString := range addrStrings {
+		addr, err := maddr.NewMultiaddr(addrString)
+		if err != nil {
+			return maddrs, err
+		}
+		maddrs = append(maddrs, addr)
+	}
+	return
+}
 
+type Config struct {
+	RendezvousString string
+	BootstrapPeers   addrList
+	ListenAddresses  addrList
+	ProtocolID       string
+}
+
+func ParseFlags() (Config, error) {
+	config := Config{}
+	flag.StringVar(&config.RendezvousString, "rendezvous", "meet me here",
+		"Unique string to identify group of nodes. Share this with your friends to let them connect with you")
+	flag.Var(&config.BootstrapPeers, "peer", "Adds a peer multiaddress to the bootstrap list")
+	flag.Var(&config.ListenAddresses, "listen", "Adds a multiaddress to the listen list")
+	flag.StringVar(&config.ProtocolID, "pid", "/chat/1.1.0", "Sets a protocol id for stream headers")
 	flag.Parse()
-	return c
+
+	if len(config.BootstrapPeers) == 0 {
+		config.BootstrapPeers = dht.DefaultBootstrapPeers
+	}
+
+	return config, nil
 }
