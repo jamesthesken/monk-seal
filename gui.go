0e065b2c8b3e263061b2a6869911a02fee300a83
James Thesken <jthesken@hawaii.edu>
2019-11-02 09:22:15 -1000

    Add logo, cleaned up user input/settings fields

--- gui.go
+++ gui.go
@@ -12,6 +12,15 @@ import (
 	"fmt"
 )
 
+
+const logo = `
+   __  ___          __     ____         __  
+  /  |/  /__  ___  / /__  / __/__ ___ _/ /  
+ / /|_/ / _ \/ _ \/  '_/ _\ \/ -_) _ / /   
+/_/  /_/\___/_//_/_/\_\ /___/\__/\_,_/_/    
+                                           
+`                                                                            
+
 // Instantiate the app
 var app = tview.NewApplication()
 
@@ -22,7 +31,8 @@ func main() {
 	newPrimitive := func(text string) tview.Primitive {
 		return tview.NewTextView().
 			SetTextAlign(tview.AlignCenter).
-			SetText(text)
+			SetText(text).
+			SetTextColor(tcell.ColorYellow).
 	}
 
 	
@@ -36,7 +46,8 @@ func main() {
 			app.Draw()
 		})
 
-	test := tview.NewTextView().
+	// Message box
+	msgField := tview.NewTextView().
 		SetTextColor(tcell.ColorYellow).
 		SetScrollable(true).
 		SetDynamicColors(true).
@@ -47,68 +58,72 @@ func main() {
 	
 // The form is getting harder to use for handling text output to the main message area
 
-/*
-	form := tview.NewForm().
-		AddInputField("> ", "", 100, nil, func(text string) {
-  			myMessage := text
-		}).
-		SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-			k := event.Key()
-			if k == tcell.KeyEnter {
-				fmt.Fprintf(test, myMessage)
-			}
-			return event
-		}).
+	mySettings := tview.NewForm().
 		AddDropDown("Channels", []string{"James", "Bob"}, 0, nil).
 		AddInputField("New Channel", "", 20, nil, nil).
 		AddButton("Connect", func() {
-			go rendezvousChat(textView, test)
+			go rendezvousChat(textView, msgField)
 		}).
 		AddButton("Quit", func() {
 			app.Stop()
 		}).
 		SetHorizontal(true);
-*/
 
-	form := tview.NewInputField().
+	myMessage := tview.NewInputField().
 			SetLabel("> ").
 			SetFieldWidth(100)
 			
 	// When user presses enter, the text clears and prints properly
-	form.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+	myMessage.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 			k := event.Key()
-			myMsg := form.GetText()
+			myMsg := myMessage.GetText()
 			if k == tcell.KeyEnter {
-				fmt.Fprintf(test,"(You) > 	")
-				fmt.Fprintf(test, myMsg)
-				fmt.Fprintf(test,"\n")
-				form.SetText("")
+				fmt.Fprintf(msgField,"(You) > 	")
+				fmt.Fprintf(msgField, myMsg)
+				fmt.Fprintf(msgField,"\n")
+				myMessage.SetText("")
 			}
 			return event
 		})
 
-	
+
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+			j := event.Key()
+
+			if j == tcell.KeyRight {
+				app.SetFocus(mySettings)
+			}else if j == tcell.KeyLeft{
+				app.SetFocus(myMessage)
+			}
+			return event
+		})
+
 	//TODO: Add a new primitive as a pointer to the handle stream funcitons of chat.go
 
 	// Grid container
 	grid := tview.NewGrid().
-		SetRows(3, 0, 3).
+		SetRows(4, 0, 4).
 		SetColumns(30, 0, 30).
 		SetBorders(true).
-		AddItem(newPrimitive("Header"), 0, 0, 1, 3, 0, 0, false)
+		AddItem(newPrimitive(logo), 0, 0, 1, 3, 0, 0, false)
 
 	//sideBar := newPrimitive("Messages")	
 		
 	// Layout for screens less than 100 cells.
 	grid.AddItem(textView, 1, 1, 1, 1, 0, 100, false).
-		AddItem(form, 2, 0, 1, 3, 0, 0, true)
+		AddItem(myMessage, 2, 0, 1, 3, 0, 0, false).
+		AddItem(mySettings, 3, 0, 1, 3, 0, 0, false).
+		AddItem(msgField, 1, 0, 1, 3, 0, 0, false)
+		
+
 	// Layout for screens wider than 100 cells.
 	grid.AddItem(textView, 1, 2, 1, 1, 0, 100, false).
-		AddItem(test, 1, 0, 1, 2, 0, 100, false).
-		AddItem(form, 2, 0, 1, 3, 0, 100, true)
+		AddItem(msgField, 1, 0, 1, 2, 0, 100, false).
+		AddItem(myMessage, 2, 0, 1, 3, 0, 100, false).
+		AddItem(mySettings, 3, 0, 1, 3, 0, 100, false)
 	
 
-	if err := app.SetRoot(grid, true).SetFocus(grid).Run(); err != nil {
+	if err := app.SetRoot(grid, true).Run(); err != nil {
 		panic(err)
 	}
 }
